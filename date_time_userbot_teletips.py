3904b07d200094f1446139638a1f65ee20cda975
risswazowlsky <85448152+risswazowlsky@users.noreply.github.com>
2022-02-22 22:58:44 +0700

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -29,7 +29,7 @@ async def main_teletips():
             if Date_Time_Userbot_teletips.is_connected:
                 Quotes_teletips = random.choice(quotes_teletips)
                 Emojis_teletips = random.choice(emojis_teletips)
-                TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
+                TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"Jakarta"))
                 Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
                 Date_teletips = TimeZone_teletips.strftime("%b %d") 
                 Image_teletips = Image.open("image.jpg")
