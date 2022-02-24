9324c5ffee901c13d0599c2879162f9252f26986
risswazowlsky <85448152+risswazowlsky@users.noreply.github.com>
2022-02-24 21:39:06 +0700

    Update date_time_userbot_teletips.py

--- date_time_userbot_teletips.py
+++ date_time_userbot_teletips.py
@@ -32,14 +32,14 @@ async def main_teletips():
                 TimeZone_teletips = datetime.datetime.now(pytz.timezone(f"{Time_Zone}"))
                 Time_teletips = TimeZone_teletips.strftime("%I:%M %p")
                 Date_teletips = TimeZone_teletips.strftime("%b %d") 
-                Image_teletips = Image.open("image2.jpg")
+                Image_teletips = Image.open("image.jpg")
                 Image_font_teletips = ImageFont.truetype("ds-digit.ttf", 360)
                 Image_text_teletips = f"{Time_teletips}"
-                Image_edit_teletips = ImageDraw.Draw(Image2_teletips)
-                Image_edit_teletips.text((690, 550), Image2_text_teletips, (0, 255, 255), font = Image_font_teletips)
+                Image_edit_teletips = ImageDraw.Draw(Image_teletips)
+                Image_edit_teletips.text((690, 550), Image_text_teletips, (0, 255, 255), font = Image_font_teletips)
                 Image_teletips.save("Image2_final_teletips.jpg")
                 await Date_Time_Userbot_teletips.update_profile(bio = f"{Emojis_teletips} {Quotes_teletips}" , last_name = f"| {Time_teletips} | {Date_teletips}")
-                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image2_final_teletips.jpg")
+                await Date_Time_Userbot_teletips.set_profile_photo(photo="Image_final_teletips.jpg")
                 me = await Date_Time_Userbot_teletips.get_me()
                 photos = await Date_Time_Userbot_teletips.get_profile_photos("me")
                 try:
